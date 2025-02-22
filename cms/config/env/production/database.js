b384a4288b5714ca2b1ea492e9e5ef771700fc69
grumpaphone <35614612+grumpaphone@users.noreply.github.com>
2025-02-22 15:51:47 -0500

    [Cursor] Update Strapi production configuration for Railway

---
+++ cms/config/env/production/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+	connection: {
+		client: 'postgres',
+		connection: {
+			host: env('PGHOST'),
+			port: env.int('PGPORT'),
+			database: env('PGDATABASE'),
+			user: env('PGUSER'),
+			password: env('PGPASSWORD'),
+			ssl: {
+				rejectUnauthorized: false,
+			},
+		},
+		debug: false,
+	},
+});
