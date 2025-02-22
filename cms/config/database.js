b50622f95b0d30ac109328ddb32459d0432a734b
grumpaphone <35614612+grumpaphone@users.noreply.github.com>
2025-02-22 17:28:19 -0500

    [Cursor] Simplify database configuration to use Railway PostgreSQL variables

--- cms/config/database.js
+++ cms/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-			host: env('DATABASE_HOST', '127.0.0.1'),
-			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'railway'),
-			user: env('DATABASE_USERNAME', 'postgres'),
-			password: env('DATABASE_PASSWORD', 'password'),
+			host: env('PGHOST', '127.0.0.1'),
+			port: env.int('PGPORT', 5432),
+			database: env('PGDATABASE', 'strapi'),
+			user: env('PGUSER', 'postgres'),
+			password: env('PGPASSWORD', ''),
 			ssl: env.bool('DATABASE_SSL', false),
 			schema: env('DATABASE_SCHEMA', 'public'),
 		},
--- cms/config/env/production/database.js
+++ cms/config/env/production/database.js
@@ -1,34 +1,15 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-	const { host, port, database, user, password } = parse(
-		env('DATABASE_URL', '')
-	);
-
-	return {
+module.exports = ({ env }) => ({
+	connection: {
+		client: 'postgres',
 		connection: {
-			client: 'postgres',
-			connection: {
-				host: host || env('PGHOST'),
-				port: port || env.int('PGPORT'),
-				database: database || env('PGDATABASE'),
-				user: user || env('PGUSER'),
-				password: password || env('PGPASSWORD'),
-				ssl: {
-					rejectUnauthorized: false,
-				},
-			},
-			debug: false,
-			acquireConnectionTimeout: 1000000,
-			pool: {
-				min: 0,
-				max: 10,
-				createTimeoutMillis: 30000,
-				acquireTimeoutMillis: 600000,
-				idleTimeoutMillis: 20000,
-				reapIntervalMillis: 20000,
-				createRetryIntervalMillis: 200,
-			},
-		},
-	};
-};
+			host: env('PGHOST'),
+			port: env.int('PGPORT'),
+			database: env('PGDATABASE'),
+			user: env('PGUSER'),
+			password: env('PGPASSWORD'),
+			ssl: {
+				rejectUnauthorized: false
+			}
+		}
+	}
+});
