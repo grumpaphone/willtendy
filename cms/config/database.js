6244a01715e1357f64f91c2c57ff523e82aee3da
grumpaphone <35614612+grumpaphone@users.noreply.github.com>
2025-02-22 14:03:05 -0500

    [Cursor] Switch to PostgreSQL and remove SQLite dependencies

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,18 +1,15 @@
 module.exports = ({ env }) => ({
 	connection: {
-		client: env('DATABASE_CLIENT', 'sqlite'),
-		connection: env('DATABASE_CLIENT', 'sqlite') === 'sqlite' 
-			? {
-				filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-			}
-			: {
-				host: env('DATABASE_HOST'),
-				port: env.int('DATABASE_PORT'),
-				database: env('DATABASE_NAME'),
-				user: env('DATABASE_USERNAME'),
-				password: env('DATABASE_PASSWORD'),
-				ssl: env.bool('DATABASE_SSL', false),
-			},
-		useNullAsDefault: true,
+		client: 'postgres',
+		connection: {
+			host: env('DATABASE_HOST'),
+			port: env.int('DATABASE_PORT', 5432),
+			database: env('DATABASE_NAME'),
+			user: env('DATABASE_USERNAME'),
+			password: env('DATABASE_PASSWORD'),
+			ssl: env.bool('DATABASE_SSL', true),
+			schema: env('DATABASE_SCHEMA', 'public'),
+		},
+		debug: false,
 	},
 });
