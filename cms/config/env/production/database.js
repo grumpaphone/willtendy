0c5fe217664cdcd0082dee738bee430996e436e0
grumpaphone <35614612+grumpaphone@users.noreply.github.com>
2025-02-22 18:23:27 -0500

    [Cursor] Update database configuration with correct Railway internal hostname

--- cms/config/env/production/database.js
+++ cms/config/env/production/database.js
@@ -17,7 +17,7 @@ module.exports = ({ env }) => {
 		connection: {
 			client: 'postgres',
 			connection: {
-				host: env('PGHOST', 'postgres.railway.internal'),
+				host: env('PGHOST', 'willtendy.railway.internal'),
 				port: env.int('PGPORT', 5432),
 				database: env('PGDATABASE', 'railway'),
 				user: env('PGUSER', 'postgres'),
